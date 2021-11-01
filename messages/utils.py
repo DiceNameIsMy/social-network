e2a6f8a61637eed3f40b7a555d130d225fe8eba1
DiceNameIsMy <dicenameismy@gmail.com>
2021-11-01 21:45:55 +0600

    minor: cosmetic changes

--- messages/utils.py
+++ messages/utils.py
@@ -1,13 +1,19 @@
 from typing import Optional
 
 from requests import request
+from requests.models import Response
 
 from models import User
 
 from settings import settings
 
 
-def send_api_request(method: str, url: str, user: Optional[User], **kwargs):
+def send_api_request(
+    method: str, 
+    url: str, 
+    user: Optional[User], 
+    **kwargs
+) -> Response:
     headers = {}
     if user is not None:
         headers['Authorization'] = f'Bearer {user.token}'
